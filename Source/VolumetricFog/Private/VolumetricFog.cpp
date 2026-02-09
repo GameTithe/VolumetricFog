daa0d9949bc72353358c2b24a20285deaa876ce9
GameTith <wkdwobum@gmail.com>
2026-02-09 20:59:39 +0900

    feat: Init Plugin

---
+++ Source/VolumetricFog/Private/VolumetricFog.cpp
@@ -0,0 +1,31 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VolumetricFog.h"
+
+#include "Interfaces/IPluginManager.h"
+#include "Misc/Paths.h"
+#include "ShaderCore.h"
+
+#define LOCTEXT_NAMESPACE "FVolumetricFogModule"
+
+void FVolumetricFogModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+	FString PluginShaderDir = FPaths::Combine(
+		IPluginManager::Get().FindPlugin(TEXT("VolumetricFog"))->GetBaseDir(),
+		TEXT("Shaders/Private")
+	);
+
+	AddShaderSourceDirectoryMapping(TEXT("/VolumetricFogPlugin"), PluginShaderDir);
+}
+
+void FVolumetricFogModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FVolumetricFogModule, VolumetricFog)
---
+++ Source/VolumetricFogEditor/Private/VolumetricFogEditor.cpp
@@ -0,0 +1,8 @@
+#include "VolumetricFogEditor.h"
+
+#define LOCTEXT_NAMESPACE "FVolumetricFogEditorEditorModule"
+
+void FVolumetricFogEditorModule::StartupModule() {}
+void FVolumetricFogEditorModule::ShutdownModule() {}
+
+#undef LOCTEXT_NAMESPACE
